c8408b4a28cdb0968124c36031908e9022d38e6c
Kunal-Paste <pastekunal6@gmail.com>
2025-11-20 18:39:31 +0530

    refactor block creation logic and improve formatting

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-const board = document.querySelector('.board');
+const board = document.querySelector(".board");
 const blockHeight = 30;
 const blockWidth = 30;
 
-const rows = Math.floor(board.clientHeight/blockHeight);
-const cols = Math.floor(board.clientWidth/blockWidth);
+const rows = Math.floor(board.clientHeight / blockHeight);
+const cols = Math.floor(board.clientWidth / blockWidth);
 
-for(let i=0; i<rows*cols; i++){
-    const block = document.createElement('div');
-    block.classList.add('block');
-    board.appendChild(block)
+for (let row = 0; row < rows; row++) {
+  for (let col = 0; col < cols; col++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    board.appendChild(block);
+    block.innerText = `${row}${col}`;
+  }
 }
